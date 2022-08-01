2497a4700ad261d40ec3d7697fd10aac8694b4f3
eliezerrb <eliezerrezende@outlook.com>
2022-08-01 20:28:08 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/eliezerrb/excliente/ExclienteApplication.java
@@ -0,0 +1,13 @@
+package com.eliezerrb.excliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExclienteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eliezerrb/excliente/ExclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliezerrb.excliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
